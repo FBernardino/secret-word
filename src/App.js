bb9769faf84c65b535523fa89d24eb203dde942a
FBernardino <filipebernardinofaria@gmail.com>
2022-08-25 18:36:02 -0300

    Salvando

---
+++ src/App.js
@@ -0,0 +1,153 @@
+//css
+import "./App.css";
+
+//react
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import { wordsList } from "./data/words";
+
+//components
+import StartScreen from "./components/StartScreen/StartScreen";
+import Game from "./components/Game/Game";
+import GameOver from "./components/GameOver/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+const guessesQty = 3;
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWords] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessesLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(guessesQty);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = useCallback(() => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  }, [words]);
+
+  //starts secret word game
+  const startGame = useCallback( () => {
+    //clear all letters
+    clearLetterStates();
+    // pick word and category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+   
+
+    // fill states
+    setPickedWords(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  }, [pickWordAndCategory]);
+
+  // process the letter input
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    // pussh guesseds letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessesLetters((actuaGuessesLetters) => [
+        ...actuaGuessesLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actuaWrongLetters) => [
+        ...actuaWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+
+  const clearLetterStates = () => {
+    setGuessesLetters([]);
+    setWrongLetters([]);
+  }
+
+
+  //check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      //resset all state
+      clearLetterStates();
+      setGameStage(stages[2].name)
+    }
+  }, [guesses]);
+
+  //check win condition
+
+  useEffect(() => {
+    const uniqueLetters =  [...new Set(letters)];
+    //win condition
+    if(guessedLetters.length === uniqueLetters.length){
+      // add scote
+      setScore((actualScore) => actualScore += 100)
+
+      //restart game with new word
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame])
+
+  // restars the   game
+  const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+    setGameStage(stages[0].name);
+  };
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          guessedLetters={guessedLetters}
+          score={score}
+        />
+      )}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
+    </div>
+  );
+}
+
+export default App;
